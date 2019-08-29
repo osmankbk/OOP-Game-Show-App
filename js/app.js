331c36572c382ff6861bacb2d57d0822098ee528
Ozman K <mackiekbk@yahoo.com>
2019-08-29 12:25:13 -0400

    created the new game in the event listener

--- js/app.js
+++ js/app.js
@@ -7,9 +7,10 @@
 const keyDiv = document.querySelector('#qwerty'); //Stored the div of the keys i'll be adding an evntListener on in a variable.
 const button = document.querySelector('#btn__reset'); //Stored the start button in a variable as well.
 const game = new Game(); //created an instance of the game Object, stored in a variable, to be used multiple times.
+let gameStart;
 button.addEventListener('click', (e) => { //Add an eventListener on the button that...
-	//const game = new Game();
-	game.startGame(); // starts a new game by call the startGame method.
+	gameStart = new Game();
+	gameStart.startGame(); // starts a new game by call the startGame method.
 });
 keyDiv.addEventListener('click', (e) => { //Added an eventListener on the div of the onscreen Keyboard and..
 	const button = e.target;
